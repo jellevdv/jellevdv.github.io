5db15c027ccba7f7be602dced42c62647190f6f8
Jelle Vandevyvere <jelle.vandevyvere@student.hogent.be>
2021-02-10 20:11:58 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+/* contact */
+//merkembtn
+document.getElementById('btn_afspraak_merkem').onclick = function () {
+  window.open(
+    'https://oc.puntoo.com/consultationbooking/index.php?key=072ee7932bd41205183c04abb1a575886f20b3195ee8eb12343b38c0e6fc67cf'
+  );
+};
+//langemarkbtn
+document.getElementById('btn_afspraak_langemark').onclick = function () {
+  window.open(
+    'https://oc.puntoo.com/patientmanagement/index.php?p=patientAgendas/key/2005a97fcdd86baf6e8d9ff812406e70ca5ddf382a0c96b380967cd95178d5e3'
+  );
+};
